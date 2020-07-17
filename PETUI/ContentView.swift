dd8d24d5444029499acbb06db7d10c5f8a67691d
leopa <leopa@leopa.com>
2020-07-17 22:51:59 +0900

    Initial Commit

---
+++ PETUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PETUI
+//
+//  Created by 高木郁哉 on 2020/07/17.
+//  Copyright © 2020 ACNNEC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
