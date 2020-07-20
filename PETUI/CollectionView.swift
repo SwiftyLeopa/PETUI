9fd86e60ade64f1bec4038e7412a13a78020ede5
rfujisaki <sax.friza2421db@gmail.com>
2020-07-20 21:54:35 +0900

    add CollectionView.swift

---
+++ PETUI/CollectionView.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionView.swift
+//  PETUI
+//
+//  Created by 藤崎嶺 on 2020/07/20.
+//  Copyright © 2020 ACNNEC. All rights reserved.
+//
+
+import SwiftUI
+
+struct Collection: View {
+    var body: some View {
+        HStack{
+            ForEach(1..<4){_ in
+            VStack{
+                ForEach(1..<6){_ in
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                    }.frame(width: 100.0, height: 100.0).accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    }
+                Spacer()
+                }
+            }
+        }.padding(.top, 60.0)
+    }
+}
+
+struct Collection_Previews: PreviewProvider {
+    static var previews: some View{
+        Collection()
+            .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+}
+}
